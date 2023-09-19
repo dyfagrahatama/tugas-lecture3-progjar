21090b984d1d7aed81d7fc0e2512f3c385ba6e85
Hibatullah Dyfa Grahatama <82629289+dyfagrahatama@users.noreply.github.com>
2023-09-19 19:15:41 +0700

    Update echoclient.c
    udapte baru

--- echoclient.c
+++ echoclient.c
@@ -1,49 +1,80 @@
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<string.h>#include<arpa/inet.h>
-#include<string.h>
-#include<arpa/inet.h>
-#include<stdio.h>
-#define MAXLINE 1024
-int main(int argc,char* argv[])
-{
-int sockfd;
-int n;
-socklen_t len;
-char sendline[1024],recvline[1024];
-struct sockaddr_in servaddr;
-sockfd=socket(AF_INET,SOCK_DGRAM,0);
-bzero(&servaddr,sizeof(servaddr));
-servaddr.sin_family=AF_INET;
-servaddr.sin_addr.s_addr=inet_addr("10.0.2.4");
-servaddr.sin_port=htons(5035);
-connect(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr));
-strcpy(sendline,"");
-printf("Welcome!!!\n\n Nama: ");
-scanf("%s",sendline);
-len=sizeof(servaddr);
-sendto(sockfd,sendline,MAXLINE,0,(struct
-sockaddr*)&servaddr,len);
-n=recvfrom(sockfd,recvline,MAXLINE,0,NULL,NULL);
-recvline[n]=0;
-printf("\n Halo! : %s\n\n",recvline);
-strcpy(sendline,"");
-printf("\n NIM : ");
-scanf("%s",sendline);
-len=sizeof(servaddr);
-sendto(sockfd,sendline,MAXLINE,0,(struct
-sockaddr*)&servaddr,len);
-n=recvfrom(sockfd,recvline,MAXLINE,0,NULL,NULL);
-recvline[n]=0;
-printf("\n NIM Bagus! : %s\n\n",recvline);
-strcpy(sendline,"Biar Semangat");
-printf("\n Harapan Untuk Hari ini : ");
-scanf("%s",sendline);
-len=sizeof(servaddr);
-sendto(sockfd,sendline,MAXLINE,0,(structsockaddr*)&servaddr,len);
-n=recvfrom(sockfd,recvline,MAXLINE,0,NULL,NULL);
-recvline[n]=0;
-printf("\n Semoga Tercapai dan Terlaksana Amin! : %s\n\n",recvline);
-return 0;
+/* 
+ * echoclient.c - A simple connection-based client
+ * usage: echoclient <host> <port>
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+#define BUFSIZE 1024
+
+/* 
+ * error - wrapper for perror
+ */
+void error(char *msg) {
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char **argv) {
+    int sockfd, portno, n;
+    struct sockaddr_in serveraddr;
+    struct hostent *server;
+    char *hostname;
+    char buf[BUFSIZE];
+
+    /* check command line arguments */
+    if (argc != 3) {
+       fprintf(stderr,"usage: %s <hostname> <port>\n", argv[0]);
+       exit(0);
+    }
+    hostname = argv[1];
+    portno = atoi(argv[2]);
+
+    /* socket: create the socket */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+
+    /* gethostbyname: get the server's DNS entry */
+    server = gethostbyname(hostname);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host as %s\n", hostname);
+        exit(0);
+    }
+
+    /* build the server's Internet address */
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+	  (char *)&serveraddr.sin_addr.s_addr, server->h_length);
+    serveraddr.sin_port = htons(portno);
+
+    /* connect: create a connection with the server */
+    if (connect(sockfd, &serveraddr, sizeof(serveraddr)) < 0) 
+      error("ERROR connecting");
+
+    /* get message line from the user */
+    printf("Please enter msg: ");
+    bzero(buf, BUFSIZE);
+    fgets(buf, BUFSIZE, stdin);
+
+    /* write: send the message line to the server */
+    n = write(sockfd, buf, strlen(buf));
+    if (n < 0) 
+      error("ERROR writing to socket");
+
+    /* read: print the server's reply */
+    bzero(buf, BUFSIZE);
+    n = read(sockfd, buf, BUFSIZE);
+    if (n < 0) 
+      error("ERROR reading from socket");
+    printf("Echo from server: %s", buf);
+    close(sockfd);
+    return 0;
 }
